524f8ca8aa8452be633ed8f9ce4610af85ef0659
nhanquoc-it <caphuunhanquoc2510@gmail.com>
2025-01-04 16:49:44 +0700

    6. config routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Import Routes
+import { publicRoutes } from '~/routes';
+// Import Layout
+import { DefaultLayout } from '~/components/Layout';
+
 function App() {
-    return <h1>React Website Tiktok UI</h1>;
+    return (
+        <Router>
+            <Routes>
+                {publicRoutes.map((route, index) => {
+                    // Config Layout
+                    let Layout = DefaultLayout;
+                    if (route.layout) {
+                        Layout = route.layout;
+                    } else if (route.layout === null) {
+                        Layout = Fragment;
+                    }
+
+                    const Page = route.component;
+
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
+                })}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Layout/index.js
@@ -0,0 +1,2 @@
+export { default as DefaultLayout } from './DefaultLayout/DefaultLayout.jsx';
+export { default as HeaderOnly } from './HeaderOnly/HeaderOnly.jsx';
